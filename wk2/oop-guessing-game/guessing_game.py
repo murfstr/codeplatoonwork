32d3773763c5e3530723d3fc07b996fd3824401c
murfstr <tristan.murphy98@outlook.com>
2024-10-15 23:48:26 -0500

    completed week1 day1 assignments

--- wk2/oop-guessing-game/guessing_game.py
+++ wk2/oop-guessing-game/guessing_game.py
@@ -1,3 +1,35 @@
 class GuessingGame():
-    # Write your code here
-    pass
+    def __init__(self, answer_number):
+        self.answer_number = answer_number
+        self.solved_status = False
+
+    def guess(self, user_guess):
+        if user_guess > self.answer_number:
+            return 'high'
+        elif user_guess == self.answer_number:
+            self.solved_status = True
+            return 'correct'
+        else:
+            return 'low'
+
+    def solved(self):
+        return self.solved_status
+
+# new_game = GuessingGame(25)
+# print(new_game.answer_number)
+
+
+# game = GuessingGame(random.randint(1,100))
+# last_guess  = None
+# last_result = None
+
+# while game.solved() == False:
+#   if last_guess != None: 
+#     print(f"Oops! Your last guess ({last_guess}) was {last_result}.")
+#     print("")
+
+#   last_guess  = input("Enter your guess: ")
+#   last_result = game.guess(last_guess)
+
+
+# print(f"{last_guess} was correct!")
--- wk2/oop-student-registry/student_registry.py
+++ wk2/oop-student-registry/student_registry.py
@@ -1,2 +1,42 @@
 class Student:
-    pass
+    def __init__(self, name, age=13, grade="12th"):
+        self._name = name
+        self._age = age
+        self._grade = grade
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if(
+            type(new_name) == str
+            and len(new_name) >= 3
+            and new_name.isalpha()
+            and new_name[0].isupper()
+        ):
+            self._name = new_name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, new_age):
+        if type(new_age) == int and new_age > 11 and new_age < 18:
+            self._age = new_age
+
+    @property
+    def grade(self):
+        return self._grade
+
+    @grade.setter
+    def grade(self, new_grade):
+        if type(new_grade) == str and (
+            new_grade == "9th"
+            or new_grade == "10th"
+            or new_grade == "11th"
+            or new_grade == "12th"
+        ):
+            self._grade = new_grade
