6462d473d17079ec8339b31892ec1aedf2e1e32a
murfstr <tristan.murphy98@outlook.com>
2024-10-17 21:52:54 -0500

    added zoo assignment

--- wk2/oop-boggle-i/boggle_board.py
+++ wk2/oop-boggle-i/boggle_board.py
@@ -1,6 +1,30 @@
+import string
+import random
+
 class BoggleBoard:
-  
+
   def __init__(self):
+    print(self)
+    user_input = input("say shake!")
+
+    if user_input == 'shake':
+      self.shake()
+    else:
+      print("that wasn't shake!")
+    
+
+
   
+  def __repr__(self):
+    return '____\n____\n____\n____'
+
 
   def shake(self):
+    for _ in range(4):
+      row = ''.join(random.choice(string.ascii_uppercase) for i in range(4))
+      print(row)
+
+
+
+# print(BoggleBoard())
+BoggleBoard()
