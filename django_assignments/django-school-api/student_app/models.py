561199640183e6a6e71bede3c4b4daea3288c7ca
murfstr <tristan.murphy98@outlook.com>
2024-11-18 22:58:11 -0600

    finished school 1

--- django_assignments/django-school-api/student_app/models.py
+++ django_assignments/django-school-api/student_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core import validators as v
 
 # Create your models here.
+class Student (models.Model):
+    name = models.CharField(blank=False)
+    student_email = models.CharField(blank=False, validators=[v.EmailValidator()])
+    personal_email = models.CharField(blank=True, validators=[v.EmailValidator()])
+    locker_number = models.IntegerField(blank=False)
+    locker_combination = models.CharField(blank=False)
+    good_student = models.BooleanField(blank=False, null=False)
